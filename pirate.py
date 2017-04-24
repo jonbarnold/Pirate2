7f4caac6be87a86d274dc02f1f085b636895f1fb
jonbarnold <jonbarnold@hotmail.com>
2017-04-24 16:05:42 -0400

    write the map file

--- pirate.py
+++ pirate.py
@@ -2,3 +2,7 @@
 
 print("arr,matey")
 #looks like my change got reverted-
+# Have pirate.py write a file
+with open('map.txt', 'w') as fh:
+  for i in range(10):
+    fh.write("Walk " + str(i) + " paces left.")
